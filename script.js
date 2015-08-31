12b8a0709347540f63edfa3e692f955e90af06f3
Kevin Ndung'u <kevgathuku@gmail.com>
2015-08-31 21:09:27 +0300

    Add function to render the middle dashed boundary

--- script.js
+++ script.js
@@ -2,18 +2,7 @@ var c = document.getElementById('myCanvas');
 var ctx = c.getContext('2d');
 var width = c.width;
 var height = c.height;
-
-// Draws the initial screen
-function initialize() {
-  ctx.fillStyle = '#000000';
-  ctx.fillRect(0, 0, width, height);
-
-  var player = new Paddle(20, 100, 20, 100);
-  var computer = new Paddle(660, 100, 20, 100);
-
-  computer.render();
-  player.render();
-}
+var middle = ((width - 5) / 2);
 
 function Paddle(x, y, width, height) {
   this.x = x;
@@ -26,4 +15,31 @@ function Paddle(x, y, width, height) {
   }
 }
 
+// Draws the initial screen
+function initialize() {
+  // Draw the canvas background
+  ctx.fillStyle = '#000000';
+  ctx.fillRect(0, 0, width, height);
+
+  // Create the paddles
+  var player = new Paddle(20, 100, 20, 100);
+  var computer = new Paddle(660, 100, 20, 100);
+
+  // Render the paddles
+  computer.render();
+  player.render();
+}
+
 initialize();
+
+// Render the middle dashed line
+function renderLineBoundary() {
+  ctx.setLineDash([15, 20]);
+  ctx.beginPath();
+  ctx.strokeStyle = '#ffffff';
+  ctx.moveTo(middle, 0);
+  ctx.lineWidth = 5;
+  ctx.lineTo(middle, height);
+  ctx.stroke();
+}
+
