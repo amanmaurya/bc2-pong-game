79879594407b24edc0fd4d03f389975a3590620d
Kevin Ndung'u <kevgathuku@gmail.com>
2015-08-31 17:28:51 +0300

    Add initialize function to draw game screen

--- script.js
+++ script.js
@@ -1,8 +1,15 @@
-var c = document.getElementById("myCanvas");
+var c = document.getElementById('myCanvas');
+var ctx = c.getContext('2d');
 var width = c.width;
 var height = c.height;
 
-var ctx = c.getContext("2d");
+// Draws the initial screen
+function initialize() {
+  ctx.fillStyle = '#000000';
+  ctx.fillRect(0, 0, width, height);
+}
 
-ctx.fillStyle = "#000000";
-ctx.fillRect(0, 0, width, height);
+// ctx.fillStyle = '#ffffff';
+// ctx.fillRect(20, 100, 20, 100);
+
+initialize();
