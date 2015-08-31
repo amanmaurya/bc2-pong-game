effad18c7eb9894fc9d5cc130f50b3be67f2680e
Kevin Ndung'u <kevgathuku@gmail.com>
2015-08-31 17:17:50 +0300

    Adjust canvas element positioning

--- script.js
+++ script.js
@@ -1,4 +1,8 @@
 var c = document.getElementById("myCanvas");
+var width = c.width;
+var height = c.height;
+
 var ctx = c.getContext("2d");
+
 ctx.fillStyle = "#000000";
-ctx.fillRect(0,0,400,590);
+ctx.fillRect(0, 0, width, height);
