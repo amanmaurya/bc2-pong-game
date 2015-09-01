b9f7d91896deefabf8aac5f0f4189f27cfe023c2
Kevin Ndung'u <kevgathuku@gmail.com>
2015-09-01 12:13:19 +0300

    Add global keypress event listeners
    The paddle moves either up or down depending on whether the up or
    down arrow is pressed

--- script.js
+++ script.js
@@ -5,7 +5,7 @@ var animate = window.requestAnimationFrame ||
               window.webkitRequestAnimationFrame ||
               window.mozRequestAnimationFrame ||
               function(callback) {
-                window.setTimeout(callback, 1000 / 60)
+                window.setTimeout(callback, 1000 / 60);
               };
 
 var c = document.getElementById('myCanvas');
@@ -13,6 +13,7 @@ var ctx = c.getContext('2d');
 var width = c.width;
 var height = c.height;
 var middle = ((width - 5) / 2);
+var keysDown = {};
 
 // Create the paddles and ball objects
 var player = new Paddle(20, 100, 20, 100); // Left Paddle
@@ -42,6 +43,7 @@ function Ball(x, y) {
       this.y = 10; // Don't go beyond the boundary
       this.y_speed = -this.y_speed; // Reverse the direction
     }
+
     // Hitting the bottom boundary
     else if (this.y - 10 > height) {
       this.y = height - 10; // Set the new position
@@ -50,14 +52,30 @@ function Ball(x, y) {
   }
 }
 
-function Paddle(x, y, width, height) {
+function Paddle(x, y, wide, long) {
   this.x = x;
   this.y = y;
-  this.width = width;
-  this.height = height;
+  this.width = wide;
+  this.height = long;
+
   this.render = function() {
     ctx.fillStyle = '#ffffff';
     ctx.fillRect(this.x, this.y, this.width, this.height);
+  }
+
+  this.move = function(x, y) {
+    this.x += x;
+    this.y += y;
+  }
+
+  this.updatePosition = function() {
+    for (var key in keysDown) {
+      if (keysDown['ArrowUp'] === true) {
+        this.move(0, -5);
+      } else if (keysDown['ArrowDown'] === true) {
+        this.move(0, 5);
+      }
+    }
   }
 }
 
@@ -84,6 +102,7 @@ function initialize() {
 
 var update = function() {
   ball.updatePosition();
+  player.updatePosition();
 };
 
 function main() {
@@ -97,3 +116,22 @@ function main() {
 window.onload = function() {
   animate(main);
 };
+
+window.addEventListener('keydown', function(event) {
+  switch (event.key) {
+    case 'ArrowUp':
+      keysDown['ArrowUp'] = true;
+      console.log(keysDown);
+      break;
+    case 'ArrowDown':
+      keysDown['ArrowDown'] = true;
+      console.log(keysDown);
+      break;
+    default:
+      return; // Do nothing
+  }
+});
+
+window.addEventListener('keyup', function(event) {
+  delete keysDown[event.key];
+});
